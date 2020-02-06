d5ff7b32795458560266c7c3be3f750d218d6250
frnhrn <henrion1311@gmail.com>
2020-02-06 23:52:24 +0200

    blocked to print xpath and class value

--- TheExtrator.py
+++ TheExtrator.py
@@ -8,19 +8,27 @@ from selenium.common.exceptions import TimeoutException
 incognito_mode = webdriver.ChromeOptions()
 incognito_mode.add_argument("--incognito")
 
+# call my Chrome driver to start my webpage analysis
 chromeDriver = webdriver.Chrome(executable_path='/Users/florianhenrion/Documents/Programming/MacSoftware/ChromeDriver/chromedriver')
 
-#chromeDriver.implicitly_wait(20) # seconds
+# looking for the macbook 13" shopping page
+
 chromeDriver.get("https://www.apple.com/be-fr/shop/buy-mac/macbook-pro/13-pouces")
 
-# Element that I am currently looking for - price of the current macbook-pro-13-in BE
-Current_price = chromeDriver.find_elements_by_class_name("as-price-currentprice")
+# get price of the current macbook-pro-13-in BE by using the class name
+
+Current_price = chromeDriver.find_elements_by_class_name("as-price-currentprice") # if you look at the first price class
+
+# try to print the value of this class which should be the price of the product
 
 for priceScan in Current_price:
     print(priceScan.get_attribute("class"))
 
-
+# waiting the page to load
 time.sleep(15)
+
+# close the browser after scan
+
 chromeDriver.quit()
 
 
--- test.py
+++ test.py
@@ -2,13 +2,18 @@ from selenium import webdriver
 import time
 #from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+# starting the driver
+driver = webdriver.Chrome() #no path as present in /bin
 driver.get("https://www.apple.com/be-fr/shop/buy-mac/macbook-pro/13-pouces")
+
+# try to find the element with the full xpath (tried with the short - didn't work too)
 
 headlines = driver.find_elements_by_xpath('/html/body/div[2]/div[7]/div[2]/bundle-selection/div[2]/h1')
 
-for headline in headlines:
-   print(headline.get_attribute('h1'))
+for headlineScan in headlines:
+   print(headlineScan.get_attribute('h1'))
+
+# currently returning the result "none"
 
 time.sleep(10)
 driver.quit()
