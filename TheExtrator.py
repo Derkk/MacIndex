ea594c618bc7f306359d7f34eb397c33bd0c799c
frnhrn <henrion1311@gmail.com>
2020-02-06 22:45:07 +0200

    end of the night

--- TheExtrator.py
+++ TheExtrator.py
@@ -1,23 +1,28 @@
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-#incognito_mode = webdriver.ChromeOptions()
-#incognito_mode.add_argument(“ — incognito”)
+incognito_mode = webdriver.ChromeOptions()
+incognito_mode.add_argument("--incognito")
 
 chromeDriver = webdriver.Chrome(executable_path='/Users/florianhenrion/Documents/Programming/MacSoftware/ChromeDriver/chromedriver')
-#chromeDriver.implicitly_wait(10) # seconds
-chromeDriver.get("https://www.apple.com/be-fr/shop/buy-iphone/iphone-11")
 
-# Element that I am currently looking for - price of the current iphone-11
+#chromeDriver.implicitly_wait(20) # seconds
+chromeDriver.get("https://www.apple.com/be-fr/shop/buy-mac/macbook-pro/13-pouces")
 
-#Current_price = chromeDriver.find_element_by_class_name('price-point price-point-fullPrice')
+# Element that I am currently looking for - price of the current macbook-pro-13-in BE
+Current_price = chromeDriver.find_elements_by_class_name("as-price-currentprice")
 
-heading1 = chromeDriver.find_element_by_tag_name('h1')
+for priceScan in Current_price:
+    print(priceScan.get_attribute("as-price-currentprice"))
 
-print('h1')
+
+time.sleep(15)
+chromeDriver.quit()
+
 
 
 
